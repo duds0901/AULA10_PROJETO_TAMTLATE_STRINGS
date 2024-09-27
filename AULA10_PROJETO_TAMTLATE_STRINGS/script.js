cfe4803c91bbaa65ca361b9f240fef095b559a14
Duda <137117671+duds0901@users.noreply.github.com>
2024-09-27 12:21:50 -0300

    "first commit"

---
+++ AULA10_PROJETO_TAMTLATE_STRINGS/script.js
@@ -0,0 +1,8 @@
+let nome = 'Ana';
+let sobrenome = 'Luiza';
+let idade = '17'
+
+console.log("Meu nome é",nome,sobrenome,"e minha idade é",idade,"anos")
+
+let mensagem = `Meu nome é ${nome} ${sobrenome} e minha idade é ${idade}anos`;
+console.log(mensagem);
